b98364522d76b244b7a757488e57d5e1c0160b12
yetone <yetoneful@gmail.com>
2024-05-04 14:39:25 +0800

    fix(ci): do not use docker

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,7 @@
 import test from 'ava'
 
-import { sum } from '../index.js'
+import { getSelectedText } from '../index.js'
 
-test('sum from native', (t) => {
-  t.is(sum(1, 2), 3)
+test('get selected text', (t) => {
+  t.is(getSelectedText(), '')
 })
